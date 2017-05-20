c3a24a12ca434051838c4d2357e097814ca3879d
BlueWinters <hanxiongxiao@gmail.com>
2017-05-20 13:19:42 +0800

    修改为虚函数

--- MatrixB.h
+++ MatrixB.h
@@ -52,20 +52,25 @@ namespace MatrixLib
 			}
 		}
 
-		inline void free()
+		virtual inline void free()
 		{
 			nRow = nCol = 0;
 			delete[] data;
 			data = NULL;
 		}
 
+		virtual inline void* datap() const
+		{
+			return (void*)data;
+		}
+
 	public:
-		inline void copy(const MatrixB& _mat)
+		virtual inline void copy(MatrixB& _mat)
 		{
 			if(_mat.assert() == true)
 			{
 				alloc(_mat.nRow, _mat.nCol);
-				memcpy(sizeof(Number)*nRow*nCol, _mat.data, data);
+				memcpy(data, _mat.data, sizeof(Number)*nRow*nCol);
 			}
 			else
 			{
@@ -74,23 +79,23 @@ namespace MatrixLib
 			}
 		}
 		
-		inline unsigned int rows()
+		virtual inline unsigned int rows()
 		{
 			return nRow;
 		}
 
-		inline unsigned int cols()
+		virtual inline unsigned int cols()
 		{
 			return nCol;
 		}
 
-		inline bool assert()
+		virtual inline bool assert()
 		{
 			// is zero matrix
 			return (nRow != 0 && nCol != 0 && data != NULL);
 		}
 
-		inline void resize(unsigned int _row, unsigned int _col)
+		virtual inline void resize(unsigned int _row, unsigned int _col)
 		{
 			if(_row == 0 || _col == 0)
 			{
@@ -102,7 +107,13 @@ namespace MatrixLib
 			}
 		}
 
-		inline bool reshape(unsigned int _row, unsigned int _col)
+		virtual inline void zero()
+		{
+			// set zero matrix
+			free();
+		}
+
+		virtual inline bool reshape(unsigned int _row, unsigned int _col)
 		{
 			if(nRow*nCol != _row*_col)
 				return false;
@@ -114,53 +125,53 @@ namespace MatrixLib
 
 	public:
 		// get value
-		inline Number at(unsigned int _n)
+		virtual inline Number at(unsigned int _n)
 		{
 			return data[_n];
 		}
 
-		inline Number at(unsigned int _i, unsigned int _j)
+		virtual inline Number at(unsigned int _i, unsigned int _j)
 		{
 			return data[nRow*_j+_i];
 		}
 
-		inline Number& atr(unsigned int _n)
+		virtual inline Number& atr(unsigned int _n)
 		{
 			return data[_n];
 		}
 
-		inline Number& atr(unsigned int _i, unsigned int _j)
+		virtual inline Number& atr(unsigned int _i, unsigned int _j)
 		{
 			return data[nRow*_j+_i];
 		}
 
-		inline Number* atp(unsigned int _n)
+		virtual inline Number* atp(unsigned int _n)
 		{
 			return &(data[_n]);
 		}
 
-		inline Number* atp(unsigned int _i, unsigned int _j)
+		virtual inline Number* atp(unsigned int _i, unsigned int _j)
 		{
 			return &(data[nRow*_j+_i]);
 		}
 
 		// set value
-		inline void set(unsigned int _n, Number _number)
+		virtual inline void set(unsigned int _n, Number _number)
 		{
 			data[_n] = _number;
 		}
 
-		inline void set(unsigned int _i, unsigned int _j, Number _number)
+		virtual inline void set(unsigned int _i, unsigned int _j, Number _number)
 		{
 			data[nRow*_j+_i] = _number;
 		}
 
-		inline void setp(unsigned int _n, Number* _number)
+		virtual inline void setp(unsigned int _n, Number* _number)
 		{
 			data[_n] = *_number;
 		}
 
-		inline void setp(unsigned int _i, unsigned int _j, Number* _number)
+		virtual inline void setp(unsigned int _i, unsigned int _j, Number* _number)
 		{
 			data[nRow*_j+_i] = *_number;
 		}
